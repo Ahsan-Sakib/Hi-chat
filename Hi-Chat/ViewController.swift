f3e109c5b3a493a2ae471c267351776fd191aed5
Ahsan <ahsanul.kabir@bjitgroup.com>
2022-11-07 14:16:10 +0600

    Initial Commit

---
+++ Hi-Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hi-Chat
+//
+//  Created by BJIT-SAKIB on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
